99eb0fdf6f918397382ef4ead68f6df1109bb050
Andrej Mihajlov <and@codeispoetry.ru>
2015-01-11 15:58:13 +0100

    Update code to return taskId

--- src/ios/BackgroundTask.m
+++ src/ios/BackgroundTask.m
@@ -3,26 +3,36 @@
 //  Copyright (c) 2015 Andrej Mihajlov
 //
 
-#import "Cordova/CDV.h"
-#import "Cordova/CDVViewController.h"
 #import "BackgroundTask.h"
 
 @implementation BackgroundTask
 
 - (void)begin:(CDVInvokedUrlCommand*)command {
-    backgroundTaskId = [[UIApplication sharedApplication] beginBackgroundTaskWithExpirationHandler:^{
-        [[UIApplication sharedApplication] endBackgroundTask:backgroundTaskId];
-        backgroundTaskId = UIBackgroundTaskInvalid;
+    __block UIBackgroundTaskIdentifier taskId;
+    
+    taskId = [[UIApplication sharedApplication] beginBackgroundTaskWithExpirationHandler:^{
+        [[UIApplication sharedApplication] endBackgroundTask:taskId];
     }];
-
-    [self.commandDelegate runInBackground:^{
-        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
-        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-    }];
+    
+    // Double should be large enough to accomodate UIBackgroundTaskIdentifier
+    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDouble:taskId];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
 - (void)end:(CDVInvokedUrlCommand*)command {
-
+    NSNumber* task = [command argumentAtIndex:0];
+    CDVPluginResult* pluginResult;
+    
+    if([task isKindOfClass:NSNumber.class]) {
+        UIBackgroundTaskIdentifier taskId = task.unsignedIntegerValue;
+        [[UIApplication sharedApplication] endBackgroundTask:taskId];
+        
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+    } else {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Invalid argument, integer expected."];
+    }
+    
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
 @end
