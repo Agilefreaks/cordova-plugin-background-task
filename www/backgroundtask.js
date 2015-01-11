c64ea2c60f9c2735421eae7a92f715d78eeae1e0
Andrej Mihajlov <and@codeispoetry.ru>
2015-01-11 16:46:20 +0100

    Update JS shim

--- www/backgroundtask.js
+++ www/backgroundtask.js
@@ -5,12 +5,12 @@ var BackgroundTask = function () {
     this.name = "BackgroundTask";
 };
 
-BackgroundTask.prototype.begin = function (task) {
-    exec(task, null, "BackgroundTask", "begin", []);
+BackgroundTask.prototype.begin = function (successFn) {
+    exec(successFn, null, "BackgroundTask", "begin", []);
 };
 
-BackgroundTask.prototype.end = function (task) {
-    exec(task, null, "BackgroundTask", "end", []);
+BackgroundTask.prototype.end = function (taskId, successFn) {
+    exec(successFn, null, "BackgroundTask", "end", [ taskId ]);
 };
 
 module.exports = new BackgroundTask();
